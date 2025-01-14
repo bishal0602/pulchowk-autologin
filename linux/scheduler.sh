8d8180e7919ae98a3fb7ebf9fcaeda19906a5c5f
tyro-apil <078bct017.apil@pcampus.edu.np>
2025-01-14 14:04:25 +0545

    feat: add scheduler shell script

---
+++ linux/scheduler.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+#1st argument: Environment file
+if [ -z "$1" ]; then
+  echo -ne "Error: No environment file provided.\n"
+  echo -ne "Usage: $0 <environment_file>\n"
+  exit 1
+fi
+
+if [ ! -f "$1" ]; then
+  echo -ne "Error: Environment file '$1' does not exist.\n"
+  exit 1
+fi
+
+source $1
+
+_temp_file="${_SCRIPT_DIR}/${_DISPATCHER_FILE}"
+
+echo -ne \
+'#!/bin/bash\n\n' > ${_temp_file}
+
+echo -ne \
+"source $1\n" >> ${_temp_file}
+
+echo -ne \
+'
+iface=$1
+action=$2
+
+_log_time=$(date +"%b %d %H:%M:%S")
+
+if [[ "$action" == "up" ]]; then
+	"$UTM_LOGIN_BIN" -username="$_UTM_USERNAME" -password="$_UTM_PASSWORD"
+
+	if [[ $? -eq 0 ]]; then
+		echo "${_log_time} Successfully logged in." >> $LOGFILE
+	else
+		echo "${_log_time} Failed to log in." >> $LOGFILE
+	fi
+fi
+
+' >> ${_temp_file}
+
+sudo chmod 755 ${_temp_file}
+
+if [[ ! -d ${_DISPATCHER_DIR} ]]; then
+  echo -ne "Dispatcher directory: ${_DISPATCHER_DIR} does not exist\n"
+  exit 1
+fi
+
+#Copy the dispatcher script to dispatcher dir
+sudo cp "${_temp_file}" "${_DISPATCHER_DIR}/${_DISPATCHER_FILE}" 
+
+sudo systemctl restart NetworkManager.service
+
+rm ${_temp_file}
+
+exit 0
