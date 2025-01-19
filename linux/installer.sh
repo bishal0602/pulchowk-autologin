1f5d3f01a3bf0ba671996f3aa7fd99a688055f2f
tyro-apil <078bct017.apil@pcampus.edu.np>
2025-01-19 13:24:37 +0545

    chore: add some print lines before and after executing commands

--- linux/installer.sh
+++ linux/installer.sh
@@ -21,12 +21,8 @@ if [[ -z ${_matched_arch} ]]; then
 	echo -ne "Your architecture is ${_user_arch} which is not supported by our script\n"
 	exit 1
 else
-	echo -ne "Proceeding with ${_user_arch}.....\n"
+	echo -ne "Proceeding with ${_user_arch}.....\n\n"
 fi
-
-
-#Install curl
-sudo apt-get -y install curl
 
 
 #Define URLs for Download
@@ -55,10 +51,10 @@ _unscheduler_path="${_script_dir}/unscheduler.sh"
 _log_path="${_script_dir}/login.log"
 
 if [[ ! -d ${_script_dir} ]]; then
-	echo -ne "Creating directory: ${_script_dir}.....\n"
+	echo -ne "Creating directory: ${_script_dir}\n\n"
 	mkdir -p ${_script_dir}
 else
-	echo -ne "${_script_dir} directory already exists.\n"
+	echo -ne "${_script_dir} directory already exists.\n\n"
 fi
 
 #Clear the environment file
@@ -83,6 +79,7 @@ download_file(){
 
 
 #Download the scripts and binaries required
+echo -ne "Downloading UTM_login binary...\n"
 _status=$(download_file $_binary_path $_target_binary_link)
 if [[ ! $_status -eq 0 ]]; then
 	echo -ne "Download failed for UTM_login binary.\n"
@@ -92,6 +89,7 @@ else
 fi
 sudo chmod 755 $_binary_path
 
+echo -ne "Downloading Scheduler script...\n"
 _status=$(download_file $_scheduler_path $_scheduler_link)
 if [[ ! $_status -eq 0 ]]; then
 	echo -ne "Download failed for Scheduler script.\n"
@@ -102,6 +100,7 @@ fi
 sudo chmod 755 $_scheduler_path
 
 
+echo -ne "Downloading Unscheduler script...\n"
 _status=$(download_file $_unscheduler_path $_unscheduler_link)
 if [[ ! $_status -eq 0 ]]; then
 	echo -ne "Download failed for Unscheduler script.\n"
@@ -129,11 +128,51 @@ echo "export _UTM_PASSWORD='${_UTM_PASSWORD}'" >> ${_env_file}
 #Run the scheduler script
 
 #Install network manager, then start and enable services (background processes)
+
+echo -ne "Installing network-manager...\n\n"
 sudo apt-get -y install network-manager
+echo -ne "\n"
+if [[ $? -eq 0 ]]; then
+	echo -ne "Successfully installed network-manager\n\n"
+else
+	echo -ne "Installation of network-manager failed\n"
+	exit 1
+fi
+
+echo -ne "Starting NetworkManager.service...\n"
 sudo systemctl start NetworkManager.service
+if [[ $? -eq 0 ]]; then
+	echo "Successfully started NetworkManager.service\n"
+else
+	echo "Starting of NetworkManager.service failed\n"
+	exit 1
+fi
 sudo systemctl enable NetworkManager.service
+sudo systemctl start NetworkManager.service
+if [[ $? -eq 0 ]]; then
+	echo "Successfully enabled NetworkManager.service\n"
+else
+	echo "Enabling NetworkManager.service failed\n"
+	exit 1
+fi
+
+echo -ne "\n"
+
+echo -ne "Starting NetworkManager-dispatcher.service...\n"
 sudo systemctl start NetworkManager-dispatcher.service
+if [[ $? -eq 0 ]]; then
+	echo -ne "Successfully started NetworkManager-dispatcher.service\n"
+else
+	echo -ne "Starting of NetworkManager-dispatcher.service failed\n"
+	exit 1
+fi
 sudo systemctl enable NetworkManager-dispatcher.service
+if [[ $? -eq 0 ]]; then
+	echo -ne "Successfully enabled NetworkManager-dispatcher.service\n"
+else
+	echo -ne "Enabling NetworkManager-dispatcher.service failed\n"
+	exit 1
+fi
 
 
 _DISPATCHER_FILE="99-pulchowk-utm-login"
@@ -145,10 +184,14 @@ echo "export _DISPATCHER_DIR='${_DISPATCHER_DIR}'" >> ${_env_file}
 sudo "${_scheduler_path}" "${_env_file}"
 
 if [[ $? -eq 0 ]]; then
+	echo -ne "\n#########################\n\n"
 	echo -ne "Successful Setup of AutoLogin script\n"
+	echo -ne "\n#########################\n\n"
 	exit 0
 else
+	echo -ne "\n#########################\n\n"
 	echo -ne "Problem with Scheduler script...\n"
 	echo -ne "Setup failed\n"
+	echo -ne "\n#########################\n\n"
 	exit 1
 fi
